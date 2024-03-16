8f17f6ef91aab1d34fb4bb3b24c314ae566f8f0e
DayanaBaron <sarmientoangie626@gmail.com>
2024-03-16 01:54:42 -0500

    Se realiza mejoras en el login y se conecta el footer con el match mejorado

--- app/src/main/java/com/example/appcitas/Login.kt
+++ app/src/main/java/com/example/appcitas/Login.kt
@@ -3,9 +3,9 @@ package com.example.appcitas
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
 import android.view.View
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
 
@@ -17,7 +17,7 @@ class Login : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login)
 
-        val BtnRegistrar = findViewById<Button>(R.id.btnRegistrar)
+        val BtnRegistrar = findViewById<ImageButton>(R.id.btnRegistrar)
         BtnRegistrar.setOnClickListener(object : View.OnClickListener{
             override fun onClick(v: View?) {
                 val intent = Intent(applicationContext,ActivityRegistrar::class.java)
@@ -25,7 +25,7 @@ class Login : AppCompatActivity() {
             }
 
         })
-        val BtnEntrar = findViewById<Button>(R.id.btnEntrarPrincipal)
+        val BtnEntrar = findViewById<ImageButton>(R.id.btnEntrarPrincipal)
         BtnEntrar.setOnClickListener(object : View.OnClickListener{
             override fun onClick(v: View?) {
                 val intent = Intent(applicationContext,Match::class.java)
@@ -34,7 +34,7 @@ class Login : AppCompatActivity() {
 
         })
 
-        val BtnVerificarCorreoContraseña=findViewById<Button>(R.id.btnEntrarPrincipal)
+        val BtnVerificarCorreoContraseña=findViewById<ImageButton>(R.id.btnEntrarPrincipal)
         BtnVerificarCorreoContraseña.setOnClickListener(object : View.OnClickListener{
             override fun onClick(v: View?) {
                 val TxtEmail = findViewById<TextView>(R.id.TxtIngresarEmail)
