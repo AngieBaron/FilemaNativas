27fc8b7842a21802f5ab05b01fd7ca43522b1992
Castellanos28 <castellanosdavid699@gmail.com>
2024-03-17 03:04:03 -0500

    Vistas de mensajes y chat

---
+++ app/src/main/java/com/example/appcitas/fragment/Mensajes.kt
@@ -0,0 +1,12 @@
+package com.example.appcitas.fragment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.appcitas.R
+
+class Mensajes : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mensajes)
+    }
+}
