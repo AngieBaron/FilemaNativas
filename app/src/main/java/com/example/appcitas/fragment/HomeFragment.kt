c56bc7e1f4dae8d3aac2da7db4bea88d109f9ed2
DayanaBaron <sarmientoangie626@gmail.com>
2024-03-19 15:13:57 -0500

    Se arregla la conexion del match con la vista de amistad

--- app/src/main/java/com/example/appcitas/fragment/HomeFragment.kt
+++ app/src/main/java/com/example/appcitas/fragment/HomeFragment.kt
@@ -1,3 +1,5 @@
+
+
 package com.example.appcitas.fragment
 
 import android.os.Bundle
@@ -11,6 +13,7 @@ import com.example.appcitas.R
 
 class HomeFragment : Fragment() {
 
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -18,26 +21,25 @@ class HomeFragment : Fragment() {
 
         // Inflate the layout for this fragment
 
-        return inflater.inflate(R.layout.fragment_match, container, false)
-
-        val view = inflater.inflate(R.layout.fragment_match, container, false)
+        val view = inflater.inflate(R.layout.fragment_match,container, false)
         // se usa para obtener una referencia al botón con el ID
         val buttonc = view.findViewById<ImageButton>(R.id.Btn_Amistad)
         buttonc.setOnClickListener {
-            //cuando se le da click al boton llama al metodo navegar
+            //    cuando se le da click al boton llama al metodo navegar
             navigateConfig()
         }
 
         return view
 
     }
-    private fun navigateConfig(){
+
+    private fun navigateConfig() {
         val activity = requireActivity() //Esta línea obtiene una referencia a la actividad que contiene este fragmento
         val fragmentManager = activity.supportFragmentManager // es responsable de administrar los fragmentos dentro de una actividad.
         //Aquí se inicia una nueva transacción de fragmentos. es decir agregar, reemplazar o quitar fragmentos, esto se realizan dentro de una transacción.
-        val fragmentTransaction =fragmentManager.beginTransaction()
+        val fragmentTransaction = fragmentManager.beginTransaction()
         //Se reemplaza el fragmento por el ID del otro fragmento
-        fragmentTransaction.replace(R.id.fragmentContainer,MatchAmistad())
+        fragmentTransaction.replace(R.id.fragmentContainer, MatchAmistad())
         //Esta línea permite al usuario retroceder a la transacción anterior al retroceder desde el dispositivo
         fragmentTransaction.addToBackStack(null)
         //se confirma la transacción, lo que efectivamente aplica todos los cambios realizados en la transacción de fragmentos
